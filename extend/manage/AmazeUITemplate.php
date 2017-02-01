4e0ba7287f7215002f503b5568150d784b4ccaf9
TerranChao <terranc@WudeMacBook-Pro.lan>
2017-02-02 00:07:33 +0800

    update

---
+++ extend/manage/AmazeUITemplate.php
@@ -0,0 +1,29 @@
+<?php
+namespace extend\manage;
+
+use Tamtamchik\SimpleFlash\TemplateInterface;
+
+/**
+ * Class Bootstrap4Template.
+ * Uses default Bootstrap 4 markdown for flash messages.
+ */
+class AmazeUITemplate extends BaseTemplate implements TemplateInterface
+{
+    protected $prefix  = '';
+    protected $postfix = '<br />';
+    protected $wrapper = '<div class="am-alert am-alert-%s" data-am-alert>%s</div>';
+    /**
+     * Override base function to suite Bootstrap 3 alert naming.
+     *
+     * @param $messages - message text
+     * @param $type     - message type: success, info, warning, error
+     *
+     * @return string
+     */
+    public function wrapMessages($messages, $type)
+    {
+        $type = ($type == 'error') ? 'danger' : $type;
+
+        return sprintf($this->getWrapper(), $type, $messages);
+    }
+}
